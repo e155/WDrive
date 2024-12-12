eca090be064754c311357c0c88b574a232b8b044
E155 <Dmytro.Drahun@outlook.com>
2024-12-12 13:22:23 +0200

    Format-Volume  -DevDrive

--- WDrive.ps1
+++ WDrive.ps1
@@ -82,7 +82,7 @@ Initialize-Disk -Number $rawDisk.Number -PartitionStyle MBR
 $partition = New-Partition -DiskNumber $rawDisk.Number -UseMaximumSize -DriveLetter $drvletter
 
 # Форматируем раздел
-Format-Volume -DriveLetter $partition.DriveLetter -FileSystem NTFS -NewFileSystemLabel "Dev_Drive"
+Format-Volume -DriveLetter $partition.DriveLetter -FileSystem NTFS -NewFileSystemLabel "Dev_Drive" -DevDrive
 
 Write-Host "Диск успешно создан и форматирован."
 $MountSuccess=$true
